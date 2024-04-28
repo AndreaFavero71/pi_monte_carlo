7534ae2de877aabaeb8c6c2ee4b5fe418dbf317a
Andrea <andrea.favero71@gmail.com>
2024-04-28 17:52:18 +0200

    Create setup.sh

---
+++ pi_monte_carlo/setup/setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+print_header () {
+  echo
+  echo $1
+  echo
+}
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (sudo $0)"
+  exit
+fi
+
+print_header "Deactivating graphical login"
+systemctl --quiet set-default multi-user.target
+
+print_header "Updating packages"
+apt update
+apt -y -qq upgrade
+
+print_header "Installing required packages"
+apt-get -y install libopencv-dev
+apt-get -y install python3-opencv
+apt-get -y install python3-matplotlib
+
+print_header "Creating python virtual env"
+python3 -m venv .virtualenvs --system-site-packages
+source .virtualenvs/bin/activate
+
+set +e
+
+print_header "Configuring vnc server"
+if ! crontab -l 2>/dev/null | grep -q "start.sh"; then
+	(crontab -l 2>/dev/null;\
+        echo -e 'MAILTO=""';\
+        echo -e '@reboot su - pi -c "/usr/bin/vncserver-virtual :1 -randr=1280x720"';\
+	| crontab -
+fi
+
+print_header "Reboot now? (y lowercase to confirm)"
+read x && [[ "$x" == "y" ]] && /sbin/reboot;
+
